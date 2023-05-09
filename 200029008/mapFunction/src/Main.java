63b4721feaa86f4528cb6a068922b5f630fcc71d
ismailkavak <118558392+ismailkavak@users.noreply.github.com>
2023-05-09 21:04:08 +0300

    created map function

---
+++ 200029008/mapFunction/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.BiFunction;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    //Maps each element of a list to a new value using the provided function.
+    public static <T, R> List<R> map(Function<T, R> func, List<T> lst) {
+
+        // Create a new list to hold the mapped values
+        List<R> result = new ArrayList<R>();
+
+        // Iterate over each element in the input list and apply the provided function
+        // to map the element to a new value of type R
+        for (T x : lst) {
+            R mappedValue = func.apply(x);
+            result.add(mappedValue);
+        }
+
+        // Return the new list of mapped values
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
