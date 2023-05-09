b6ceda8bf1ef7f745f89c4bd46671e7baa68713d
ismailkavak <118558392+ismailkavak@users.noreply.github.com>
2023-05-09 23:36:49 +0300

    created partition function test

--- 200029008/mapFunction/src/Test.java
+++ 200029008/mapFunction/src/Test.java
@@ -32,7 +32,10 @@ public class Test {
         int result = composed.apply(5);
         System.out.println(result);
 
-        
+        // Test partition function
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15);
+        List<List<Integer>> partitioned = Main.partition(integers, n -> n % 2 == 0);
+        System.out.println(partitioned);
     }
 
 }
