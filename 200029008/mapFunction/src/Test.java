21a2142a49260710b5a3d2786fa05a86ff38bc9c
ismailkavak <118558392+ismailkavak@users.noreply.github.com>
2023-05-09 22:48:15 +0300

    created filter function test

--- 200029008/mapFunction/src/Test.java
+++ 200029008/mapFunction/src/Test.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.BiFunction;
 
 public class Test {
     public static void main(String[] args) {
@@ -9,6 +12,21 @@ public class Test {
         Function<Integer, Integer> square = x -> x * x;
         List<Integer> output = Main.map(square, input);
         System.out.println(output);
+
+
+        // Test filter function
+
+        List<String> words = Arrays.asList("GTR R35", "SUPRA", "R34", "SPIDER 458", "R8");
+        List<String> filteredWords = Main.filter(word -> word.length() > 4, words);
+        System.out.println(filteredWords);
+
+        
+
+
     }
 
 }
+
+
+
+
