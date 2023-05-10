c1251eb14b1aa9e402e2552d98700658d5289cb0
ismailkavak <118558392+ismailkavak@users.noreply.github.com>
2023-05-10 12:57:52 +0300

    I changed file name

---
+++ 200029008/highOrderFunctions/src/Main.java
@@ -0,0 +1,91 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.BiFunction;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    //Maps each element of a list to a new value using the provided function.
+    public static <T, R> List<R> map(Function<T, R> func, List<T> lst) {
+
+        // Create a new list to hold the mapped values
+        List<R> result = new ArrayList<R>();
+
+        // Iterate over each element in the input list and apply the provided function
+        // to map the element to a new value of type R
+        for (T x : lst) {
+            R mappedValue = func.apply(x);
+            result.add(mappedValue);
+        }
+
+        // Return the new list of mapped values
+        return result;
+    }
+    //Filters the elements of a list using the provided predicate.
+    public static <T> List<T> filter(Predicate<T> pred, List<T> lst) {
+
+        // Create a new list to hold the filtered elements
+        List<T> filteredList = new ArrayList<T>();
+
+        // Iterate over each element in the input list and apply the provided predicate
+        // to determine if the element should be included in the filtered list
+        for (T item : lst) {
+            if (pred.test(item)) {
+                filteredList.add(item);
+            }
+        }
+
+        // Return the new list of filtered elements
+        return filteredList;
+    }
+
+    // Define a generic method named reduce that takes a binary function, a list of elements, and an optional initial value
+    public static <T> T reduce(BiFunction<T, T, T> function, List<T> list, T initialValue) {
+
+        // Initialize an accumulator variable to the initial value (or the first element of the list if no initial value is provided)
+        T accumulator = initialValue != null ? initialValue : list.get(0);
+
+        // Iterate over the remaining elements of the list and apply the binary function to the accumulator and the current element
+        for (int i = 1; i < list.size(); i++) {
+            T element = list.get(i);
+            accumulator = function.apply(accumulator, element);
+        }
+
+        // Return the accumulated result
+        return accumulator;
+    }
+
+    // Define a method named compose that takes two Function objects and returns their composition
+    public static <T, U, V> Function<T, V> compose(Function<U, V> g, Function<T, U> f) {
+        return x -> g.apply(f.apply(x));
+    }
+    public static <T> List<List<T>> partition(List<T> list, Predicate<T> predicate) {
+        List<List<T>> result = new ArrayList<>();
+        List<T> trueList = new ArrayList<>();
+        List<T> falseList = new ArrayList<>();
+        for (T elem : list) {
+            if (predicate.test(elem)) {
+                trueList.add(elem);
+            } else {
+                falseList.add(elem);
+            }
+        }
+        result.add(trueList);
+        result.add(falseList);
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
